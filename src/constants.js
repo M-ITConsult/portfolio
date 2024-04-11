fb87bffa3a909834a61d6df557c7997b8a504bba
M-ITConsult <jchoro74@gmail.com>
2024-04-11 15:04:00 +0200

    update

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC.`,
+    tv: `TV`,
+    bed: `BED`,
+    resume: `RESUME`,
+    projects: `PROJECTS`,
+    library: `LIBRARY`,
+    sofa: `SOFA`,
+}
