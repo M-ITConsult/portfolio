d2eaa607bc90a99989d411b0609b0fda3c370471
M-ITConsult <jchoro74@gmail.com>
2024-04-11 19:52:29 +0200

    update

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const dialogueData = {
     <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org</a>, <a href="https://www.youtube.com/@ErikDubois" target="_blank">Arcolinux with Erik Dubois</a>,
    <a href="https://www.youtube.com/@Underscore_" target="_blank">Underscore_ with Micode and his team</a> and <a href="https://www.youtube.com/@CodingAddict/featured" target="_blank">Coding Addict with John Smilga</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <br /><a href="https://github.com/M-ITConsult/portfolio/blob/main/Jean-Christophe%20MILER_Resume_Portfolio.pdf" target="_blank">Check it out?</a>
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/M-ITConsult/portfolio/blob/main/Jean-Christophe%20MILER_Resume_Portfolio.pdf" target="_blank">Check it out?</a>
     I'm currently available for opportunities, so contact me at jeanchristophemiler@hotmail.com`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
