7356b0217b46d7636ebfbf90ec744a4b2b2c96b3
Jean-Christophe <74435332+M-ITConsult@users.noreply.github.com>
2024-09-24 12:47:16 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,20 +2,20 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
     pc: `This is my PC. I'm learning CyberSecurity & Ethical Hacking these days.
-    I've made an ecommerce with java/angular and springboot. I also like Python and Kotlin. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/M-ITConsult" target="_blank">Github</a>!`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-    <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org</a>, <a href="https://www.youtube.com/@ErikDubois" target="_blank">Arcolinux with Erik Dubois</a>,
-   <a href="https://www.youtube.com/@Underscore_" target="_blank">Underscore_ with Micode and his team</a> and <a href="https://www.youtube.com/@CodingAddict/featured" target="_blank">Coding Addict with John Smilga</a>!`,
+    I'm learning and practical skills on <a href="https://tryhackme.com/p/Jchoro" target="_blank">TryHackMe</a> < you can check my profile btw and HackTheBox.
+    If you want check my projects > <a href="https://github.com/M-ITConsult" target="_blank">Github</a>!`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like:
+    <a href="https://www.youtube.com/@davidbombal" target="_blank">David Bombal</a>, <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a> and,
+   <a href="https://www.youtube.com/@Underscore_" target="_blank">Underscore_ with Micode and his team</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
     resume: `This is my desk and on it is my resume. <a href="https://github.com/M-ITConsult/portfolio/blob/main/Jean-Christophe%20MILER_Resume_Portfolio.pdf" target="_blank">Check it out?</a>
     I'm currently available for opportunities, so contact me at jeanchristophemiler@hotmail.com`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching some of tutorials from the author JSLegendDev <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in English (I also speak English btw).
+    library: `There are a lot of tech books on my shelves. There is even one in English (I also speak English btw).
     I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    'sofa-table': `That's my sofa. I like to relax here and watch movies sometimes.`,
+    'sofa-table': `That's my sofa. I like to relax here and watch movies or tv shows sometimes like Mr. Robot :).`,
     'cadre-mur': `Actually, I have two certifications <a href="https://github.com/M-ITConsult/portfolio/blob/main/Certification_C3SA.pdf" target="_blank">Here <a href="https://github.com/M-ITConsult/portfolio/blob/main/Ethical_Hacking_Intro.pdf" target="_blank">& Here`,
     exit: `If you want to exit Jean-Christophe Miler's portfolio, just close the tab.`,
 }
