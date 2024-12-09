bf707bfc9e32e0d95a01018f22bd4010064b2a72
Jean-Christophe <74435332+M-ITConsult@users.noreply.github.com>
2024-12-09 20:25:26 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -8,8 +8,7 @@ export const dialogueData = {
     <a href="https://www.youtube.com/@davidbombal" target="_blank">David Bombal</a>, <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a> and,
    <a href="https://www.youtube.com/@Underscore_" target="_blank">Underscore_ with Micode and his team</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/M-ITConsult/portfolio/blob/main/Jean-Christophe%20MILER_Resume_Portfolio.pdf" target="_blank">Check it out?</a>
-    I'm currently available for opportunities, so contact me at jeanchristophemiler@hotmail.com`,
+    resume: `This is my desk and on it is my TryHackMe profile. <a href="https://tryhackme.com/r/p/Jchoro" target="_blank">Check it out?</a>`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching some of tutorials from the author JSLegendDev <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
